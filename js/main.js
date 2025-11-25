aeaa369461c24ff499ee3d5d971b5630739dd73b
Sergii Taran <serg.taran1970@gmail.com>
2025-11-25 20:26:11 +0200

    feat: add Accordion.js initialization and smooth expand animation for FAQ section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import { initTheme } from "./modules/theme.js";
 import { initMenu } from "./modules/menu.js";
 import { initProjects } from "./modules/projects.js";
+import { initQuestionsAccordion } from "./modules/questions.js";
 
 initTheme();
 initMenu();
 initProjects();
+initQuestionsAccordion();
